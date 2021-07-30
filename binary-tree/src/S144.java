ac938a72196517eff755ec5f807456292dd7ebf9
lm <super1742720898@gmail.com>
2021-07-31 01:10:57 +0800

    initial commit

---
+++ binary-tree/src/S144.java
@@ -0,0 +1,14 @@
+import javax.swing.tree.TreeNode;
+import java.util.List;
+
+/**
+ * 144 前序遍历
+ *
+ * @author ZhangFuQi
+ * @date 2021/7/31 1:02
+ */
+public class S144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
+}
---
+++ binary-tree/src/TreeNode.java
@@ -0,0 +1,24 @@
+/**
+ * 二叉树定义
+ *
+ * @author ZhangFuQi
+ * @date 2021/7/31 1:04
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
