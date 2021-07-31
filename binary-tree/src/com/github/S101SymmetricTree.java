a1bc78475d63bc5c6287a3451f6cd3655a517397
lm <super1742720898@gmail.com>
2021-07-31 15:55:33 +0800

    对称二叉树

--- binary-tree/src/com/github/S101SymmetricTree.java
+++ binary-tree/src/com/github/S101SymmetricTree.java
@@ -8,6 +8,22 @@ package com.github;
  */
 public class S101SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
-        return true;
+        return symmetric(root.left, root.right);
+    }
+
+    private boolean symmetric(TreeNode left, TreeNode right) {
+        //左右都不为空，则比较数值，不相等就返回false
+        if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            //左右都为空返回true
+        } else if (left == null && right == null) {
+            return true;
+            //左右有一个不为空，返回false
+        } else {
+            return false;
+        }
+        return symmetric(left.left, right.right) && symmetric(left.right, right.left);
     }
 }
