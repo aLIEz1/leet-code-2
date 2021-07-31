ea41ca0489de9ca282a8dc7a2b8f3df61c16b3b0
lm <super1742720898@gmail.com>
2021-07-31 21:00:43 +0800

    二叉树的所有路径

--- binary-tree/src/com/github/S257BinaryTreePaths.java
+++ binary-tree/src/com/github/S257BinaryTreePaths.java
@@ -1,5 +1,9 @@
 package com.github;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 二叉树的所有路径
  *
@@ -7,4 +11,32 @@ package com.github;
  * @date 2021/7/31 18:40
  */
 public class S257BinaryTreePaths {
+    List<String> ans = new ArrayList<>();
+    LinkedList<String> path = new LinkedList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        path(root);
+        return ans;
+    }
+
+    private void path(TreeNode root) {
+        path.add(root.val + "->");
+        if (root.left == null && root.right == null) {
+            StringBuilder sb = new StringBuilder();
+            for (String s : path) {
+                sb.append(s);
+            }
+            sb.delete(sb.length() - 2, sb.length());
+            ans.add(sb.toString());
+            return;
+        }
+        if (root.left != null) {
+            path(root.left);
+            path.removeLast();
+        }
+        if (root.right != null) {
+            path(root.right);
+            path.removeLast();
+        }
+    }
 }
