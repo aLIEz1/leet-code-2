bdd2124e1b199ac5d0f95a1cb257181ab1377301
lm <super1742720898@gmail.com>
2021-08-01 23:08:52 +0800

    滑动窗口 长度最小的子数组

---
+++ array/src/com/github/S209MinimumSizeSubarraySum.java
@@ -0,0 +1,32 @@
+package com.github;
+
+/**
+ * 长度最小的子数组
+ *
+ * @author ZhangFuQi
+ * @date 2021/8/1 22:21
+ */
+public class S209MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int length = Integer.MAX_VALUE;
+        while (right < nums.length) {
+            //窗口增长
+            int rightSideView = nums[right];
+            right++;
+            if (sum < target) {
+                sum += rightSideView;
+            }
+            //窗口缩小，取最优解
+            while (sum >= target) {
+                length = Math.min(right - left, length);
+                int leftSideView = nums[left];
+                left++;
+                sum -= leftSideView;
+            }
+        }
+        return length == Integer.MAX_VALUE ? 0 : length;
+    }
+}
