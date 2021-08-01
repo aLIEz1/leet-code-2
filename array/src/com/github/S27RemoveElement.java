454d64c0084b39ea153b0c9c1c09ca1441538bc8
lm <super1742720898@gmail.com>
2021-08-01 21:45:46 +0800

    二分查找，双指针

---
+++ array/src/com/github/S27RemoveElement.java
@@ -0,0 +1,30 @@
+package com.github;
+
+/**
+ * 移除元素
+ *
+ * @author ZhangFuQi
+ * @date 2021/8/1 21:13
+ */
+public class S27RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+        int fastIndex = 0;
+        int slowIndex = 0;
+        while (fastIndex < length) {
+            if (nums[fastIndex] != val) {
+                nums[slowIndex] = nums[fastIndex];
+                slowIndex++;
+            }
+            fastIndex++;
+        }
+        return slowIndex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new S27RemoveElement().removeElement(new int[]{3, 2, 2, 3}, 3));
+    }
+}
---
+++ array/src/com/github/S704BinarySearch.java
@@ -0,0 +1,44 @@
+package com.github;
+
+/**
+ * 二分查找
+ * 注意left和right一开始的取值，right能不能取到
+ * 能取到while循环中就能等于，不能取到while循环中就不能等于
+ * 能取到右边可以等于mid-1,因为除去了mid,不能取到右边要等于mid因为mid取不到，小于相当于取了mid-1;
+ *
+ * @author ZhangFuQi
+ * @date 2021/8/1 20:31
+ */
+public class S704BinarySearch {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
