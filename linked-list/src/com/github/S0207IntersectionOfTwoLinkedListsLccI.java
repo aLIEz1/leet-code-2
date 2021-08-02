08f77b75cf65dc010d436f07221906d6a957d927
lm <super1742720898@gmail.com>
2021-08-02 22:19:38 +0800

    链表相交

---
+++ linked-list/src/com/github/S0207IntersectionOfTwoLinkedListsLccI.java
@@ -0,0 +1,53 @@
+package com.github;
+
+/**
+ * 链表相交
+ *
+ * @author ZhangFuQi
+ * @date 2021/8/2 21:45
+ */
+public class S0207IntersectionOfTwoLinkedListsLccI {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        // 求链表A的长度
+        while (curA != null) {
+            lenA++;
+            curA = curA.next;
+        }
+        // 求链表B的长度
+        while (curB != null) {
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if (lenB > lenA) {
+            //1. swap (lenA, lenB);
+            int tmpLen = lenA;
+            lenA = lenB;
+            lenB = tmpLen;
+            //2. swap (curA, curB);
+            ListNode tmpNode = curA;
+            curA = curB;
+            curB = tmpNode;
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+}
