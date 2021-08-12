ecd2caaecdd1dafaf07c2ec3455d1426b36c3ed0
lm <super1742720898@gmail.com>
2021-08-13 01:21:05 +0800

    回文链表

---
+++ linked-list/src/com/github/S234PalindromeLinkedList.java
@@ -0,0 +1,40 @@
+package com.github;
+
+/**
+ * 回文链表
+ *
+ * @author ZhangFuQi
+ * @date 2021/8/13 1:13
+ */
+public class S234PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode reverse = reverse(slow);
+
+        while (reverse != null && head != null) {
+            if (reverse.val != head.val) {
+                return false;
+            }
+            reverse = reverse.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
