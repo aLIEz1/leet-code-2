5d485c4caef9e50f779602de438580c3f5d0a4de
lm <super1742720898@gmail.com>
2021-08-21 21:34:06 +0800

    复原ip地址

--- backtracking/src/com/github/S93RestoreIpAddresses.java
+++ backtracking/src/com/github/S93RestoreIpAddresses.java
@@ -10,21 +10,51 @@ import java.util.List;
  * @date 2021/8/15 22:42
  */
 public class S93RestoreIpAddresses {
+
+    private static final int SEG_COUNT = 4;
+    private final List<String> ans = new ArrayList<>();
+    private int[] segment;
+
     public List<String> restoreIpAddresses(String s) {
-        List<String> ans = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        backtracking(s, ans, sb, 0);
+        segment = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
     }
 
-    private void backtracking(String s, List<String> ans, StringBuilder sb, int startIndex) {
-        if (startIndex > s.length()) {
+    private void dfs(String s, int segId, int segStart) {
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < SEG_COUNT; i++) {
+                    sb.append(segment[i]);
+                    if (i != SEG_COUNT - 1) {
+                        sb.append(".");
+                    }
+                }
+                ans.add(sb.toString());
+            }
             return;
         }
-        if (sb.length() == s.length() + 3) {
-            ans.add(sb.toString());
+        //对应没找到4段ip就遍历完了字符串的情况
+        if (segStart == s.length()) {
+            return;
         }
-        for (int i = startIndex; i < s.length(); i++) {
+        //如果当前数字为0，则这一段ip地址只能为0
+        if (s.charAt(segStart) == '0') {
+            segment[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
 
+        //对应一般情况
+        int address = 0;
+        for (int i = segStart; i < s.length(); i++) {
+            address = address * 10 + (s.charAt(i) - '0');
+            if (address > 0 && address <= 0xFF) {
+                segment[segId] = address;
+                dfs(s, segId + 1, i + 1);
+            } else {
+                break;
+            }
         }
     }
 }
